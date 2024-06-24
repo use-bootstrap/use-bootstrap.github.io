18ceea74f47e29cbecef850ad983fb38df2839a9
Erwin Heldy <erwin.heldy@gmail.com>
2024-06-24 21:19:28 +0700

    added use-bootstrap-dialog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,12 @@ import pkg from '../../package.json'
 import style from '../style.css.txt'
 import script from '../script.js.txt'
 
+function UseBootstrapDialogIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M8 9h8" /><path d="M8 13h6" /><path d="M9 18h-3a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-3l-3 3l-3 -3z" /></svg>
+  )
+}
+
 function UseBootstrapSelectIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 11V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h6"></path><path d="m12 12 4 10 1.7-4.3L22 16Z"></path></svg>
@@ -18,6 +24,12 @@ function UseBootstrapToasterIcon() {
 
 export default function () {
   const items = [
+    {
+      icon: <UseBootstrapDialogIcon />,
+      name: 'use-bootstrap-dialog',
+      desc: 'A JavaScript library for generating dialogs using Bootstrap modals',
+      link: 'https://use-bootstrap-dialog.js.org',
+    },
     {
       icon: <UseBootstrapSelectIcon />,
       name: 'use-bootstrap-select',
